82fa0959b9bec5edd5073738f6375def7263a402
zhangwei <zhangwei>
2019-10-16 16:28:32 +0800

    代理模式

--- Java-design-pattrens/src/structure/proxy/Proxy.java
+++ Java-design-pattrens/src/structure/proxy/Proxy.java
@@ -1,5 +1,9 @@
 package structure.proxy;
 
+/**
+ * 代理
+ * 提供了与真实主题相同的接口，其内部含有对真实主题的引用，它可以访问、控制或扩展真实主题的功能
+ */
 public class Proxy implements Subject{
     private RealSubject realSubject;
     @Override
