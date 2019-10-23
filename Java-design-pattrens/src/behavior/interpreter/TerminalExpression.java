07a404cd4f8e74374e73478cbe882b57ee87d257
zhangwei <zhangwei>
2019-10-23 09:49:54 +0800

    解释器模式

---
+++ Java-design-pattrens/src/behavior/interpreter/TerminalExpression.java
@@ -0,0 +1,12 @@
+package behavior.interpreter;
+
+/**
+ * 终结符表达式类
+ *
+ */
+public class TerminalExpression implements AbstractExpression {
+    @Override
+    public Object interpret(String info) {
+        return null;
+    }
+}
