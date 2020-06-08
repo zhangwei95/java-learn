5101d417417ba1491955f906f1cba32879257a89
zhangwei95 <574150903@qq.com>
2020-06-08 17:01:34 +0800

    逃逸分析  栈上分配  TLAB

---
+++ java-jvm/src/gc/TLABTest.java
@@ -0,0 +1,38 @@
+package gc;
+
+
+/**
+ * 逃逸分析 栈上分配  减少GC压力
+ * 不开启逃逸分析
+ * -XX:-DoEscapeAnalysis -XX:-EliminateAllocations -XX:-UseTLAB -Xlog:c5_gc*
+ * 开启逃逸分析
+ * -XX:+DoEscapeAnalysis
+ *  逃逸分析 标量替换 线程专有对象分配
+ */
+public class TLABTest {
+
+    class User {
+        int id;
+        String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    void alloc(int i) {
+        new User(i, "name " + i);
+    }
+
+
+    public static void main(String[] args) {
+        TLABTest t = new TLABTest();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000_0000; i++) t.alloc(i);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+
+        //for(;;);
+    }
+}
