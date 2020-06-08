c88fe40cdb9638a3efb9e99a80b57cd628fb7739
zhangwei95 <574150903@qq.com>
2020-06-08 16:11:57 +0800

    指令重排

--- java-jvm/src/PossibleReordering.java
+++ java-jvm/src/PossibleReordering.java
@@ -5,9 +5,19 @@ public class PossibleReordering {
     static int x = 0, y = 0;
     static int a = 0, b = 0;
     public static void main(String[] args) throws InterruptedException {
-        for (int i= 0;i<1000;i++){
+        int i = 0;
+        for(;;) {
+            i++;
+            x = 0; y = 0;
+            a = 0; b = 0;
             Thread one = new Thread(new Runnable() {
                 public void run() {
+                    //由于线程one先启动，下面这句话让它等一等线程two. 读着可根据自己电脑的实际性能适当调整等待时间.
+                    shortWait(100000);
+
+                    //memory = allocate();       //1：分配对象的内存空间 count = 0;
+                    //ctorInstance(memory);     //2：初始化 count = 10
+                    //instance = memory;         //3：设置instance指向刚分配的内存地址
                     a = 1;
                     x = b;
                 }
@@ -21,15 +31,22 @@ public class PossibleReordering {
             });
             one.start();other.start();
             one.join();other.join();
-            System.out.println("(" + x + "," + y + ")");
-            x = 0; y = 0;
-            a = 0; b = 0;
+            String result = "第" + i + "次 (" + x + "," + y + ")";
+            if(x == 0 && y == 0) {
+                System.err.println(result);
+                break;
+            } else {
+                //System.out.println(result);
+            }
         }
+    }
 
-//        a = 1;
-//        x = b;
-//        b = 1;
-//        y = a;
-        System.out.println("(" + x + "," + y + ")");
+
+    public static void shortWait(long interval){
+        long start = System.nanoTime();
+        long end;
+        do{
+            end = System.nanoTime();
+        }while(start + interval >= end);
     }
 }
