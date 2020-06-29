5af9b60622a24dff038b497e138127fb29375f44
zhangwei95 <574150903@qq.com>
2020-06-29 11:24:07 +0800

    冒泡 对数器

--- java-algorithm/src/sort/bubble/BubbleSort.java
+++ java-algorithm/src/sort/bubble/BubbleSort.java
@@ -13,21 +13,42 @@ public class BubbleSort {
     //冒泡排序
     public static void bubbleSort(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
-            int flag = 0;
+            boolean flag = false; // 是否交换 没交换待办已经排好序了
             for (int j = 0; j < nums.length - 1 - i; j++) {
                 int temp = nums[j];
                 if (nums[j] > nums[j + 1]) {
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
-                    flag = 1;
+                    flag = true;
                 }
             }
-            System.out.print("冒泡第" + i + "趟排序：");
-            SortMethod.printArray(nums);
-            if (flag == 0) {
+//            System.out.print("冒泡第" + i + "趟排序：");
+//            SortMethod.printArray(nums);
+            if (!flag) {
                 return;
             }
         }
+//        SortMethod.printArray(nums);
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100000;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = SortMethod.generateRandomArray(maxSize, maxValue);
+            int[] arr2 = SortMethod.copyArray(arr1);
+            bubbleSort(arr1);
+            SortMethod.comparator(arr2);
+            if (!SortMethod.isEqual(arr1, arr2)) {
+                succeed = false;
+                SortMethod.printArray(arr1);
+                SortMethod.printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
     }
 
 }
