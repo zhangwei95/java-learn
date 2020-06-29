80a1700c4879ed8f7c621726dd52cf80bb6bd6f5
zhangwei95 <574150903@qq.com>
2020-06-29 11:24:28 +0800

    插入排序 对数器

--- java-algorithm/src/sort/insert/BinaryInsertSort.java
+++ java-algorithm/src/sort/insert/BinaryInsertSort.java
@@ -36,8 +36,28 @@ public class BinaryInsertSort {
                 // 插入有序数列
                 nums[left] = temp;
             }
-            System.out.print("二分插入第" + i + "趟排序：");
-            SortMethod.printArray(nums);
+//            System.out.print("二分插入第" + i + "趟排序：");
+//            SortMethod.printArray(nums);
         }
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100000;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = SortMethod.generateRandomArray(maxSize, maxValue);
+            int[] arr2 = SortMethod.copyArray(arr1);
+            binaryInsertSort(arr1);
+            SortMethod.comparator(arr2);
+            if (!SortMethod.isEqual(arr1, arr2)) {
+                succeed = false;
+                SortMethod.printArray(arr1);
+                SortMethod.printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
     }
 }
